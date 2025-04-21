4662f0902b0035da77de019a5fccd968f996a606
VladimirKozlov73 <postwsz@mail.ru>
2025-04-21 14:56:32 +0300

    Добавлен второй вариант решения задачи 2.

--- src/Main.java
+++ src/Main.java
@@ -20,9 +20,25 @@ public class Main {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
             }
         } else if (clientOS == 1) {
+                System.out.println("Установите версию приложения для Android по ссылке.");
+            } else
+                System.out.println("Установите версию приложения для iOS по ссылке.");
+        System.out.println();
+
+        //Второй вариант
+        System.out.println("Второй вариант:");
+        if (clientDeviceYear > 2015 && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        }
+        if(clientDeviceYear > 2015 && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        }
+        if (clientDeviceYear <= 2015 && clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке.");
-        } else
+        }
+        if (clientDeviceYear <= 2015 && clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
+        }
         System.out.println();
 
         //Задача 3
